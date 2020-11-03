0dbb9ed9f6084d7ec6790a3d978e1146ae50ba60
Chloe6700 <denis.chloe@gmail.com>
2020-11-03 16:25:42 +0000

    commit

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
-var colors;
-colors = ['red' , 'orange' , 'yellow' , 'green' , 'blue' , 'indigo' , 'violet'];
+var firstName;
+firstName = 'Chloe';
 
-console.log(colors);
+var favouriteColor;
+favouriteColor = ['cyan' , 'lightgreen'];
+
+var favouriteFood = 'Cakes';
+var city = "Paris";
+
+console.log(firstName + ' ' + favouriteColor + ' ' + favouriteFood + ' ' + city);
+
+var phrase = 'A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all.';
+var endingPhrase = 'is what we see. Rainbow colours, are made with light bending you see!';
+var one = 'Red';
+var two = 'orange';
+var three = 'yellow';
+var four = 'green';
+var five = 'blue';
+var six = 'indigo';
+var seven = 'violet';
+
+console.log(phrase + ' ' + one + ' ' + two + ' ' + three + ' ' + four + ' ' + five + ' ' + six + ' ' + seven + ' ' + endingPhrase);
