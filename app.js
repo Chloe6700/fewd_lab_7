1461e9f92a558eeab6da76f241fe264b1823f785
Chloe6700 <denis.chloe@gmail.com>
2020-11-08 10:51:22 +0000

    commit

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var city = "Paris";
 
 console.log(firstName + ' ' + favouriteColor + ' ' + favouriteFood + ' ' + city);
 
+/* Question 9: The code write the var value in the console log, after renning the code. */
+
 var phrase = 'A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all.';
 var endingPhrase = 'is what we see. Rainbow colours, are made with light bending you see!';
 var one = 'Red';
@@ -20,3 +22,6 @@ var six = 'indigo';
 var seven = 'violet';
 
 console.log(phrase + ' ' + one + ' ' + two + ' ' + three + ' ' + four + ' ' + five + ' ' + six + ' ' + seven + ' ' + endingPhrase);
+
+var myAlphabetLength = ['A', 'B', 'C', 'D','E','F', 'G'];
+console.log (myAlphabetLength.length);
